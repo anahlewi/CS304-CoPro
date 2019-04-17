3aa18bbc7d383566c59e2ab6ec0fd97955c6df90
anahlewi <alewi@wellesley.edu>
2019-04-17 16:34:31 +0000

    added templates and routes

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import (Flask, url_for, render_template, request, redirect, flash)
+import random,math
+import movieDatabase
+app = Flask(__name__)
+
+app.secret_key = ''.join([ random.choice(('ABCDEFGHIJKLMNOPQRSTUVXYZ' +
+                                          'abcdefghijklmnopqrstuvxyz' +
+                                          '0123456789'))
+                           for i in range(20) ])
+                           
+conn = movieDatabase.getConn('c9')
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+    
+@app.route("/dashboard/<user>")
+def dashboard(user):
+    return render_template("dashboard.html")
+    
+@app.route("/match/<user>")
+def dashboard(user):
+    return render_template("match.html")
+    
