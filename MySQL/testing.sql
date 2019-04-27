af3e1ecbd69554fce44c228e5f6e132f8736ec62
Daphka Alius <dalius@wellesley.edu>
2019-04-27 00:25:09 +0000

    Batch file for testing tables

---
+++ MySQL/testing.sql
@@ -0,0 +1,14 @@
+-- Testing Codes
+
+-- Users
+insert into users(username, bnumber, name, email, phone, userType, resHall, 
+availability) values ('dalius', 'B20800497', 'Daphka Alius', 
+'dalius@wellesley.edu', '786-317-0380', 'Student', 'Tower Court West');
+
+insert into users(username, bnumber, name, email, phone, userType, resHall, 
+availability) values ('lanah', 'B20800000', 'Anah Lewi', 
+'lanahs@wellesley.edu', '123-456-7890', 'Student', 'Stone Hall');
+
+insert into users(username, bnumber, name, email, phone, userType, resHall, 
+availability) values ('mhardy', 'B20800001', 'Miranda Hardy', 
+'mhardy@wellesley.edu', '098-765-4321', 'Student', 'Tower Court East');
