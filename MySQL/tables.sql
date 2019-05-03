9b3b2e25fcf2d7fe5b3643b6d9d10e9ab3582497
Daphka Alius <dalius@wellesley.edu>
2019-05-03 13:57:15 +0000

    Updated tables 5/3

--- MySQL/tables.sql
+++ MySQL/tables.sql
@@ -45,9 +45,9 @@ create table if not exists courses(
 	courseName varchar(60),
 	instructor varchar(9),
 	semester varchar(7), 
-	foreign key (instructor) references users(bnumber) on update cascade,
-	primary key (courseNum))
-	ENGINE	= InnoDB;
+	foreign key (instructor) references users(bnumber),
+	primary key (courseNum));
+	-- ENGINE	= InnoDB;
 
 create table if not exists psets(
 	pid int auto_increment,
@@ -55,38 +55,33 @@ create table if not exists psets(
 	dueDate date,
 	maxSize int,
 	courseNum int, 
-	foreign key (courseNum) references courses(courseNum) 
-	on delete cascade on update cascade,
-	primary key (pid))
-	ENGINE = InnoDB;
+	foreign key (courseNum) references courses(courseNum),
+	primary key (pid));
+	-- ENGINE = InnoDB;
 	
 	
 create table if not exists groups(
 	groupNum int auto_increment NOT NULL,
 	pid int,
 	courseNum int,
-	foreign key (pid) references psets(pid) 
-	on delete cascade on update cascade,
-	foreign key (courseNum) references courses(courseNum) 
-	on delete cascade on update cascade,
-	primary key (groupNum))
-	ENGINE	= InnoDB;
+	foreign key (pid) references psets(pid),
+	foreign key (courseNum) references courses(courseNum),
+	primary key (groupNum));
+	-- ENGINE	= InnoDB;
 
 
 create table if not exists groupForPset(
 	groupNum int,
 	bnumber varchar(9),
-	foreign key (groupNum) references groups(groupNum) 
-	on delete cascade on update cascade,
-	foreign key (bnumber) references users(bnumber) 
-	on delete cascade on update cascade,
-	primary key (groupNum))
-	ENGINE	= InnoDB;
+	foreign key (groupNum) references groups(groupNum),
+	foreign key (bnumber) references users(bnumber),
+	primary key (groupNum, bnumber));
+	-- ENGINE	= InnoDB;
 	
 create table if not exists enrollment(
-	BNumber varchar(9) references users(bnumber) 
-	on delete cascade on update cascade,
-	courseNum int references courses(courseNum) 
-	on delete cascade on update cascade,
-	primary key (bnumber, courseNum))
-	ENGINE	= InnoDB;
+	courseNum int,
+	bnumber varchar(9),
+	foreign key(courseNum)  references courses(courseNum),
+	foreign key(bnumber) references users(bnumber),
+	primary key (bnumber, courseNum));
+	-- ENGINE	= InnoDB;
